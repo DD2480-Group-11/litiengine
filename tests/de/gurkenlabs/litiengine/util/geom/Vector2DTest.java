2199ead8058ce433aa506fd17508bee1476de93e
Yu Zhou <yuzho@kth.se>
2021-02-26 20:21:30 +0100

    Add a new unit test.

--- tests/de/gurkenlabs/litiengine/util/geom/Vector2DTest.java
+++ tests/de/gurkenlabs/litiengine/util/geom/Vector2DTest.java
@@ -8,18 +8,24 @@ public class Vector2DTest {
     /**
      * Test dotProduct in Vector2D class
      */
+    /*
     @Test
     public void dotProductTest(){
         Vector2D v1 = new Vector2D(1,2);
         Vector2D v2 = new Vector2D(3,4);    
         assertEquals(11, v1.dotProduct(v2),0.001);
     }
+    */
+    
+    @Test
     public void addTest(){
         Vector2D v1 = new Vector2D(1,2);
         Vector2D v2 = new Vector2D(3,4);
         Vector2D v3 = new Vector2D(4,6);
-        boolean isTrue = v3==v1.add(v2);
+        boolean isTrue = false;
+        if(v3.dX == v1.add(v2).dX && v3.dY == v1.add(v2).dY) isTrue = true;
         assertTrue(isTrue);
 
     }
+    
 }
