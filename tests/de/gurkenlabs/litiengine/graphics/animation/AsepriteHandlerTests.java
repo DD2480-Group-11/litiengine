54ed3ab036160da9b9c36bbdb32079a8baaa188f
HannesSundin <hannessu@kth.se>
2021-03-09 16:25:45 +0100

    Updated formatting
    4 space tabs -> 2 space tabs

--- tests/de/gurkenlabs/litiengine/graphics/animation/AsepriteHandlerTests.java
+++ tests/de/gurkenlabs/litiengine/graphics/animation/AsepriteHandlerTests.java
@@ -13,36 +13,32 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class AsepriteHandlerTests {
 
+	/**
+	 * Test that just create a json and prints in to standard output.
+	 */
+	@Test
+	public void exportAnimationTest() {
+		String spritesheetPath = "C:/Users/Nikla/Documents/Programmering/SoftwareFundamentals/Assignment-3-EC/litiengine/tests/de/gurkenlabs/litiengine/graphics/animation/aseprite_test_animation/Sprite-0001-sheet.png";
+		BufferedImage image = new BufferedImage(96, 32, BufferedImage.TYPE_4BYTE_ABGR);
+		Spritesheet spritesheet = new Spritesheet(image, spritesheetPath, 32, 32);
+		String result = AsepriteHandler.exportAnimation(new SpritesheetResource(spritesheet));
+		System.out.println(result);
+	}
 
-    /**
-     * Test that just create a json and prints in to standard output.
-     */
-    @Test
-    public void exportAnimationTest() {
-        String spritesheetPath = "C:/Users/Nikla/Documents/Programmering/SoftwareFundamentals/Assignment-3-EC/litiengine/tests/de/gurkenlabs/litiengine/graphics/animation/aseprite_test_animation/Sprite-0001-sheet.png";
-        BufferedImage image = new BufferedImage(96, 32, BufferedImage.TYPE_4BYTE_ABGR);
-        Spritesheet spritesheet = new Spritesheet(image, spritesheetPath, 32, 32);
-        AsepriteHandler aseprite = new AsepriteHandler();
-        String result = aseprite.exportAnimation(new SpritesheetResource(spritesheet));
-        System.out.println(result);
-    }
+	@Test
+	public void importAnimationTest() {
+		try {
+			Animation animation = AsepriteHandler.importAnimation("tests/de/gurkenlabs/litiengine/graphics/animation/aseprite_test_animation/Sprite-0001.json");
+			assertEquals(300, animation.getTotalDuration());
+			assertEquals("Sprite-0001-sheet", animation.getName());
 
-    @Test
-    public void importAnimationTest() {
-        try {
-            Animation animation = AsepriteHandler.importAnimation("tests/de/gurkenlabs/litiengine/graphics/animation/aseprite_test_animation/Sprite-0001.json");
-            assertEquals(300, animation.getTotalDuration());
-            assertEquals("Sprite-0001-sheet", animation.getName());
-
-            Spritesheet spriteSheet = animation.getSpritesheet();
-            assertEquals(32, spriteSheet.getSpriteHeight());
-            assertEquals(32, spriteSheet.getSpriteWidth());
-            assertEquals(3, spriteSheet.getTotalNumberOfSprites());
-            assertEquals(ImageFormat.PNG, spriteSheet.getImageFormat());
-        } catch (FileNotFoundException e) {
-            fail(e.getMessage());
-        } catch (AsepriteHandler.ImportAnimationException e) {
-            fail(e.getMessage());
-        }
-    }
+			Spritesheet spriteSheet = animation.getSpritesheet();
+			assertEquals(32, spriteSheet.getSpriteHeight());
+			assertEquals(32, spriteSheet.getSpriteWidth());
+			assertEquals(3, spriteSheet.getTotalNumberOfSprites());
+			assertEquals(ImageFormat.PNG, spriteSheet.getImageFormat());
+		} catch (FileNotFoundException | AsepriteHandler.ImportAnimationException e) {
+			fail(e.getMessage());
+		}
+	}
 }
