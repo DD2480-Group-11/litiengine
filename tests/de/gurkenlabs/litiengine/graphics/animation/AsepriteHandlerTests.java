29e8dc18b25f8bfa11bce6bebb1d0aa76f261680
HannesSundin <hannessu@kth.se>
2021-03-09 12:48:56 +0100

    Update AsepriteHandlerTests.java
    Update to accomodate new changes to exportAnimation()

--- tests/de/gurkenlabs/litiengine/graphics/animation/AsepriteHandlerTests.java
+++ tests/de/gurkenlabs/litiengine/graphics/animation/AsepriteHandlerTests.java
@@ -1,31 +1,24 @@
 package de.gurkenlabs.litiengine.graphics.animation;
 
-import java.io.FileNotFoundException;
-import java.awt.image.BufferedImage;
-
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
+import de.gurkenlabs.litiengine.resources.SpritesheetResource;
 import org.junit.jupiter.api.Test;
 
-import de.gurkenlabs.litiengine.graphics.Spritesheet;
-import de.gurkenlabs.litiengine.graphics.animation.Animation;
+import java.awt.image.BufferedImage;
 
 public class AsepriteHandlerTests {
 
 
   /**
-   * Test that just create a json and prints in to standard output. 
+   * Test that just create a json and prints in to standard output.
    */
   @Test
   public void exportAnimationTest() {
     String spritesheetPath = "C:/Users/Nikla/Documents/Programmering/SoftwareFundamentals/Assignment-3-EC/litiengine/tests/de/gurkenlabs/litiengine/graphics/animation/aseprite_test_animation/Sprite-0001-sheet.png";
     BufferedImage image = new BufferedImage(96, 32, BufferedImage.TYPE_4BYTE_ABGR);
     Spritesheet spritesheet = new Spritesheet(image, spritesheetPath, 32, 32);
-    Animation animation = new Animation(spritesheet, false, false, 2,2,2);
-
     AsepriteHandler aseprite = new AsepriteHandler();
-    String result = aseprite.exportAnimation(animation);
+    String result = aseprite.exportAnimation(new SpritesheetResource(spritesheet));
     System.out.println(result);
 
   }
